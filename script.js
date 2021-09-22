475a332634c431a5f98acdacd47ca4f49a814946
Abhishekreju2525 <abhishekreju007@gmail.com>
2021-09-22 10:27:31 +0530

    title update

--- script.js
+++ script.js
@@ -3,12 +3,3 @@ window.onscroll = () => {
   if(this.scrollY <= 1) nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-t'; else nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-tt';
 };
 
-window.onscroll = () => {
-  const nav = document.querySelector('#navbarTogglerDemo02');
-  if(this.scrollY <= 1) nav.className = 'collapse navbar-collapse bg-color'; else nav.className = 'collapse navbar-collapse bg-ttt';
-};
-
-
-  
-  
-  collapse navbar-collapse bg-ttt
