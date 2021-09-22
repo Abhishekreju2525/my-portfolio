9cc4a94d528a3b66209eca90a109116cbffb116f
Abhishekreju2525 <abhishekreju007@gmail.com>
2021-09-22 11:21:40 +0530

    commit

--- script.js
+++ script.js
@@ -3,4 +3,3 @@ window.onscroll = () => {
   if(this.scrollY <= 1) nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-t'; else nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-tt';
 };
 
-<i class="bi bi-x-lg"></i>
