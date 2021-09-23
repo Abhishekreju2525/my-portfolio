b3f0aa7924d9bdfb29ef8b5eefbac98bd26943c5
Abhishekreju2525 <abhishekreju007@gmail.com>
2021-09-23 10:17:22 +0530

    skills added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onscroll = () => {
   const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 1) nav.className = 'navbar navbar-expand-lg fixed-top navbar-light bg-t'; else nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-tt';
+  if(this.scrollY <= 1) nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-t'; else nav.className = 'navbar navbar-expand-lg fixed-top navbar-dark bg-tt';
 };
 
